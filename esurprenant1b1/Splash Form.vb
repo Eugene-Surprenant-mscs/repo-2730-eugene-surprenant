b10aa04799491ae2167406770864f4c55ddb6adc
sureug <sureug@WINLPC00X00J>
2016-08-25 12:52:19 -0500

    Add project files.

---
+++ esurprenant1b1/Splash Form.vb
@@ -0,0 +1,26 @@
+﻿Public Class frmSplash
+    Private Sub frmSplash_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub lblName_Click(sender As Object, e As EventArgs) Handles lblName.Click
+
+    End Sub
+
+    Private Sub lblMsg_Click(sender As Object, e As EventArgs) Handles lblMsg.Click
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles picIguanas.Click
+
+    End Sub
+
+    Private Sub tmrExit_Tick(sender As Object, e As EventArgs) Handles tmrExit.Tick
+        Me.Close()
+
+    End Sub
+End Class
