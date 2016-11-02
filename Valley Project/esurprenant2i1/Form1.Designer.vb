c61d069cb892a750ee56dd03271b3507c9d2df4e
Eugene-Surprenant-mscs <eugene.surprenant@my.southeastmn.edu>
2016-11-02 17:41:56 -0500

    Added assignment 2i1

---
+++ Valley Project/esurprenant2i1/Form1.Designer.vb
@@ -0,0 +1,451 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.txt03 = New System.Windows.Forms.TextBox()
+        Me.txt04 = New System.Windows.Forms.TextBox()
+        Me.txt05a = New System.Windows.Forms.TextBox()
+        Me.txt5b = New System.Windows.Forms.TextBox()
+        Me.txt05c = New System.Windows.Forms.TextBox()
+        Me.txt05d = New System.Windows.Forms.TextBox()
+        Me.txt05e = New System.Windows.Forms.TextBox()
+        Me.txt06 = New System.Windows.Forms.TextBox()
+        Me.txt07 = New System.Windows.Forms.TextBox()
+        Me.txt12 = New System.Windows.Forms.TextBox()
+        Me.txt13 = New System.Windows.Forms.TextBox()
+        Me.lbl03 = New System.Windows.Forms.Label()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.lbl04 = New System.Windows.Forms.Label()
+        Me.lbl05a = New System.Windows.Forms.Label()
+        Me.lbl05b = New System.Windows.Forms.Label()
+        Me.lbl05c = New System.Windows.Forms.Label()
+        Me.lbl05d = New System.Windows.Forms.Label()
+        Me.lbl05e = New System.Windows.Forms.Label()
+        Me.lbl06 = New System.Windows.Forms.Label()
+        Me.lbl07 = New System.Windows.Forms.Label()
+        Me.lbl12 = New System.Windows.Forms.Label()
+        Me.lbl13 = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(27, 24)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(137, 21)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "03 Remove/Insert:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(20, 59)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(102, 21)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "04 Substring:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(20, 94)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(91, 21)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "05a Lenght:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(20, 129)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(76, 21)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "05b Trim:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(20, 164)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(144, 21)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "05c Remove/insert:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(20, 202)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(83, 21)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "05d Insert:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(20, 237)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(69, 21)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "05e Pad:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(20, 272)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(92, 21)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "06 Remove:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(20, 307)
+        Me.Label9.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(89, 21)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "07 Contain:"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(20, 342)
+        Me.Label10.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(123, 21)
+        Me.Label10.TabIndex = 9
+        Me.Label10.Text = "12 Len/Remove:"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(20, 377)
+        Me.Label11.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(63, 21)
+        Me.Label11.TabIndex = 10
+        Me.Label11.Text = "13 Like:"
+        '
+        'txt03
+        '
+        Me.txt03.Location = New System.Drawing.Point(192, 21)
+        Me.txt03.Name = "txt03"
+        Me.txt03.Size = New System.Drawing.Size(171, 29)
+        Me.txt03.TabIndex = 11
+        Me.txt03.Text = "late"
+        '
+        'txt04
+        '
+        Me.txt04.Location = New System.Drawing.Point(192, 56)
+        Me.txt04.Name = "txt04"
+        Me.txt04.Size = New System.Drawing.Size(171, 29)
+        Me.txt04.TabIndex = 12
+        Me.txt04.Text = "YMBlueX"
+        '
+        'txt05a
+        '
+        Me.txt05a.Location = New System.Drawing.Point(192, 91)
+        Me.txt05a.Name = "txt05a"
+        Me.txt05a.Size = New System.Drawing.Size(171, 29)
+        Me.txt05a.TabIndex = 13
+        Me.txt05a.Text = "Disk full"
+        '
+        'txt5b
+        '
+        Me.txt5b.Location = New System.Drawing.Point(192, 126)
+        Me.txt5b.Name = "txt5b"
+        Me.txt5b.Size = New System.Drawing.Size(171, 29)
+        Me.txt5b.TabIndex = 14
+        Me.txt5b.Text = "   Red Wing   "
+        '
+        'txt05c
+        '
+        Me.txt05c.Location = New System.Drawing.Point(192, 164)
+        Me.txt05c.Name = "txt05c"
+        Me.txt05c.Size = New System.Drawing.Size(171, 29)
+        Me.txt05c.TabIndex = 15
+        Me.txt05c.Text = "cater"
+        '
+        'txt05d
+        '
+        Me.txt05d.Location = New System.Drawing.Point(192, 199)
+        Me.txt05d.Name = "txt05d"
+        Me.txt05d.Size = New System.Drawing.Size(171, 29)
+        Me.txt05d.TabIndex = 16
+        Me.txt05d.Text = "day"
+        '
+        'txt05e
+        '
+        Me.txt05e.Location = New System.Drawing.Point(192, 234)
+        Me.txt05e.Name = "txt05e"
+        Me.txt05e.Size = New System.Drawing.Size(171, 29)
+        Me.txt05e.TabIndex = 17
+        Me.txt05e.Text = "765.44"
+        '
+        'txt06
+        '
+        Me.txt06.Location = New System.Drawing.Point(192, 269)
+        Me.txt06.Name = "txt06"
+        Me.txt06.Size = New System.Drawing.Size(171, 29)
+        Me.txt06.TabIndex = 18
+        Me.txt06.Text = "3,123,560"
+        '
+        'txt07
+        '
+        Me.txt07.Location = New System.Drawing.Point(192, 304)
+        Me.txt07.Name = "txt07"
+        Me.txt07.Size = New System.Drawing.Size(171, 29)
+        Me.txt07.TabIndex = 19
+        Me.txt07.Text = "308 JEfFeRsOn StReEt"
+        '
+        'txt12
+        '
+        Me.txt12.Location = New System.Drawing.Point(192, 339)
+        Me.txt12.Name = "txt12"
+        Me.txt12.Size = New System.Drawing.Size(171, 29)
+        Me.txt12.TabIndex = 20
+        Me.txt12.Text = "5.0%"
+        '
+        'txt13
+        '
+        Me.txt13.Location = New System.Drawing.Point(192, 374)
+        Me.txt13.Name = "txt13"
+        Me.txt13.Size = New System.Drawing.Size(171, 29)
+        Me.txt13.TabIndex = 21
+        Me.txt13.Text = "60500"
+        '
+        'lbl03
+        '
+        Me.lbl03.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl03.Location = New System.Drawing.Point(399, 21)
+        Me.lbl03.Name = "lbl03"
+        Me.lbl03.Size = New System.Drawing.Size(171, 29)
+        Me.lbl03.TabIndex = 23
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(192, 417)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(75, 29)
+        Me.btnDisplay.TabIndex = 22
+        Me.btnDisplay.Text = "Display"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'lbl04
+        '
+        Me.lbl04.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl04.Location = New System.Drawing.Point(399, 56)
+        Me.lbl04.Name = "lbl04"
+        Me.lbl04.Size = New System.Drawing.Size(171, 29)
+        Me.lbl04.TabIndex = 24
+        '
+        'lbl05a
+        '
+        Me.lbl05a.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl05a.Location = New System.Drawing.Point(399, 91)
+        Me.lbl05a.Name = "lbl05a"
+        Me.lbl05a.Size = New System.Drawing.Size(171, 29)
+        Me.lbl05a.TabIndex = 25
+        '
+        'lbl05b
+        '
+        Me.lbl05b.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl05b.Location = New System.Drawing.Point(399, 126)
+        Me.lbl05b.Name = "lbl05b"
+        Me.lbl05b.Size = New System.Drawing.Size(171, 29)
+        Me.lbl05b.TabIndex = 26
+        '
+        'lbl05c
+        '
+        Me.lbl05c.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl05c.Location = New System.Drawing.Point(399, 163)
+        Me.lbl05c.Name = "lbl05c"
+        Me.lbl05c.Size = New System.Drawing.Size(171, 29)
+        Me.lbl05c.TabIndex = 27
+        '
+        'lbl05d
+        '
+        Me.lbl05d.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl05d.Location = New System.Drawing.Point(399, 199)
+        Me.lbl05d.Name = "lbl05d"
+        Me.lbl05d.Size = New System.Drawing.Size(171, 29)
+        Me.lbl05d.TabIndex = 28
+        '
+        'lbl05e
+        '
+        Me.lbl05e.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl05e.Location = New System.Drawing.Point(399, 234)
+        Me.lbl05e.Name = "lbl05e"
+        Me.lbl05e.Size = New System.Drawing.Size(171, 29)
+        Me.lbl05e.TabIndex = 29
+        '
+        'lbl06
+        '
+        Me.lbl06.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl06.Location = New System.Drawing.Point(399, 269)
+        Me.lbl06.Name = "lbl06"
+        Me.lbl06.Size = New System.Drawing.Size(171, 29)
+        Me.lbl06.TabIndex = 30
+        '
+        'lbl07
+        '
+        Me.lbl07.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl07.Location = New System.Drawing.Point(399, 304)
+        Me.lbl07.Name = "lbl07"
+        Me.lbl07.Size = New System.Drawing.Size(171, 29)
+        Me.lbl07.TabIndex = 31
+        '
+        'lbl12
+        '
+        Me.lbl12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl12.Location = New System.Drawing.Point(399, 339)
+        Me.lbl12.Name = "lbl12"
+        Me.lbl12.Size = New System.Drawing.Size(171, 29)
+        Me.lbl12.TabIndex = 32
+        '
+        'lbl13
+        '
+        Me.lbl13.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lbl13.Location = New System.Drawing.Point(399, 374)
+        Me.lbl13.Name = "lbl13"
+        Me.lbl13.Size = New System.Drawing.Size(171, 29)
+        Me.lbl13.TabIndex = 33
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(399, 417)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 29)
+        Me.btnExit.TabIndex = 34
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 21.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(624, 460)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lbl13)
+        Me.Controls.Add(Me.lbl12)
+        Me.Controls.Add(Me.lbl07)
+        Me.Controls.Add(Me.lbl06)
+        Me.Controls.Add(Me.lbl05e)
+        Me.Controls.Add(Me.lbl05d)
+        Me.Controls.Add(Me.lbl05c)
+        Me.Controls.Add(Me.lbl05b)
+        Me.Controls.Add(Me.lbl05a)
+        Me.Controls.Add(Me.lbl04)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.lbl03)
+        Me.Controls.Add(Me.txt13)
+        Me.Controls.Add(Me.txt12)
+        Me.Controls.Add(Me.txt07)
+        Me.Controls.Add(Me.txt06)
+        Me.Controls.Add(Me.txt05e)
+        Me.Controls.Add(Me.txt05d)
+        Me.Controls.Add(Me.txt05c)
+        Me.Controls.Add(Me.txt5b)
+        Me.Controls.Add(Me.txt05a)
+        Me.Controls.Add(Me.txt04)
+        Me.Controls.Add(Me.txt03)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Exercise 2i1: String methods"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents txt03 As TextBox
+    Friend WithEvents txt04 As TextBox
+    Friend WithEvents txt05a As TextBox
+    Friend WithEvents txt5b As TextBox
+    Friend WithEvents txt05c As TextBox
+    Friend WithEvents txt05d As TextBox
+    Friend WithEvents txt05e As TextBox
+    Friend WithEvents txt06 As TextBox
+    Friend WithEvents txt07 As TextBox
+    Friend WithEvents txt12 As TextBox
+    Friend WithEvents txt13 As TextBox
+    Friend WithEvents lbl03 As Label
+    Friend WithEvents btnDisplay As Button
+    Friend WithEvents lbl04 As Label
+    Friend WithEvents lbl05a As Label
+    Friend WithEvents lbl05b As Label
+    Friend WithEvents lbl05c As Label
+    Friend WithEvents lbl05d As Label
+    Friend WithEvents lbl05e As Label
+    Friend WithEvents lbl06 As Label
+    Friend WithEvents lbl07 As Label
+    Friend WithEvents lbl12 As Label
+    Friend WithEvents lbl13 As Label
+    Friend WithEvents btnExit As Button
+End Class
---
+++ esurprenant2i1/Form1.vb
@@ -0,0 +1,108 @@
+﻿'Name:         Exercise 2I
+' Purpose:      String Methods
+' Programmer:   Eugene Surprenant on 11/02/2016
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        '#03: Insert
+
+        Dim strWord As String = txt03.Text
+        strWord = strWord.Remove(0, 1)
+        strWord = strWord.Insert(0, "cr")
+        lbl03.Text = strWord
+
+        '#04: Substring
+
+        Dim strItem As String = txt04.Text
+        strItem = strItem.Substring(2, 4)
+        lbl04.Text = strItem
+
+        '#05a: Lenght
+
+        Dim strMsg As String = txt05a.Text
+        strMsg = CType(strMsg.Length, String)
+        lbl05a.Text = strMsg
+
+        '#05b: Trim
+
+        Dim strState As String = txt5b.Text
+        strState = txt5b.Text.Trim
+        lbl05b.Text = strState
+
+        '#5c: Remove/Insert
+
+        Dim strwrd As String = txt05c.Text
+        strwrd = strwrd.Remove(1, 1)
+        strwrd = strwrd.Insert(1, "rit")
+        lbl05c.Text = strwrd
+
+        '#05d: Insert
+
+        Dim strDay As String
+        strDay = txt05d.Text
+        strDay = strDay.Insert(0, "Mon")
+        lbl05d.Text = strDay
+
+        '#05e: Pad
+        Dim strPay As String
+        strPay = txt05e.Text
+        lbl05e.Text = strPay.PadLeft(10, "*"c)
+
+        '#06: Remove
+
+        Dim strAmount As String
+        strAmount = txt06.Text
+        strAmount = strAmount.Remove(1, 1)
+        strAmount = strAmount.Remove(4, 1)
+        lbl06.Text = strAmount
+
+        '#07: Contains
+
+        Dim strAddress As String
+        Dim blnIsContained As Boolean
+        strAddress = txt07.Text
+        blnIsContained = strAddress.ToLower.Contains("jefferson street")
+        If blnIsContained Then
+            lbl07.Text = "Jeff St. Found"
+        Else
+            lbl07.Text = "Jeff St. Not found"
+        End If
+
+        '#12: Len/Remove
+
+        Dim strRate As String
+        Dim intLength As Integer
+        Dim dblTax As Double
+        Dim dblRate As Double
+        strRate = txt12.Text
+        intLength = strRate.Length
+        strRate = strRate.Remove(intLength - 2, 2)
+        Double.TryParse(strRate, dblRate)
+        lbl12.Text = (dblRate * 1000 * 0.01).ToString("c2")
+
+        '#13: Like
+
+        Dim strZip As String
+        strZip = txt13.Text
+        If strZip Like "605##" Then
+            lbl13.Text = "Shipping $25"
+        ElseIf strZip Like "606##" Then
+            lbl13.Text = "Shipping $30"
+        Else
+            lbl13.Text = "Invalid Zip"
+        End If
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
