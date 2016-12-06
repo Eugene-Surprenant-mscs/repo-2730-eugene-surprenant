6767d11da2a23d2e33f32a6632945cc34efbae35
Eugene-Surprenant-mscs <eugene.surprenant@my.southeastmn.edu>
2016-12-06 17:01:06 -0600

    Added assignment 3e1

---
+++ esurprenant3e1/Form1.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cboSearchValues = New System.Windows.Forms.ComboBox()
+        Me.cboSearchRanges = New System.Windows.Forms.ComboBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.lblRangeCount = New System.Windows.Forms.Label()
+        Me.lblValueCount = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblValues = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cboSearchValues
+        '
+        Me.cboSearchValues.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple
+        Me.cboSearchValues.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboSearchValues.FormattingEnabled = True
+        Me.cboSearchValues.Items.AddRange(New Object() {"50", "100", "300", "900"})
+        Me.cboSearchValues.Location = New System.Drawing.Point(139, 134)
+        Me.cboSearchValues.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.cboSearchValues.Name = "cboSearchValues"
+        Me.cboSearchValues.Size = New System.Drawing.Size(107, 126)
+        Me.cboSearchValues.TabIndex = 20
+        '
+        'cboSearchRanges
+        '
+        Me.cboSearchRanges.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple
+        Me.cboSearchRanges.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboSearchRanges.FormattingEnabled = True
+        Me.cboSearchRanges.Items.AddRange(New Object() {"    0 - 200", "100 - 300", "700 - 800"})
+        Me.cboSearchRanges.Location = New System.Drawing.Point(139, 276)
+        Me.cboSearchRanges.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
+        Me.cboSearchRanges.Name = "cboSearchRanges"
+        Me.cboSearchRanges.Size = New System.Drawing.Size(107, 126)
+        Me.cboSearchRanges.TabIndex = 19
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(250, 280)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(45, 17)
+        Me.Label7.TabIndex = 18
+        Me.Label7.Text = "Count:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(31, 280)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(112, 17)
+        Me.Label8.TabIndex = 17
+        Me.Label8.Text = "Search for Range:"
+        '
+        'lblRangeCount
+        '
+        Me.lblRangeCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblRangeCount.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblRangeCount.Location = New System.Drawing.Point(298, 280)
+        Me.lblRangeCount.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblRangeCount.Name = "lblRangeCount"
+        Me.lblRangeCount.Size = New System.Drawing.Size(61, 24)
+        Me.lblRangeCount.TabIndex = 16
+        '
+        'lblValueCount
+        '
+        Me.lblValueCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblValueCount.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblValueCount.Location = New System.Drawing.Point(298, 134)
+        Me.lblValueCount.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblValueCount.Name = "lblValueCount"
+        Me.lblValueCount.Size = New System.Drawing.Size(61, 24)
+        Me.lblValueCount.TabIndex = 15
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(250, 136)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(45, 17)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "Count:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(31, 134)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(107, 17)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Search for Value:"
+        '
+        'lblValues
+        '
+        Me.lblValues.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblValues.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblValues.Location = New System.Drawing.Point(153, 25)
+        Me.lblValues.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.lblValues.Name = "lblValues"
+        Me.lblValues.Size = New System.Drawing.Size(207, 74)
+        Me.lblValues.TabIndex = 12
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(31, 25)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(124, 17)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Commission Values:"
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(298, 331)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 32)
+        Me.btnExit.TabIndex = 21
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(432, 413)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.cboSearchValues)
+        Me.Controls.Add(Me.cboSearchRanges)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.lblRangeCount)
+        Me.Controls.Add(Me.lblValueCount)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblValues)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "esurprenant3e1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cboSearchValues As ComboBox
+    Friend WithEvents cboSearchRanges As ComboBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents lblRangeCount As Label
+    Friend WithEvents lblValueCount As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblValues As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnExit As Button
+End Class
